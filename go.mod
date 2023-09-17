3d707e59929eeb32882fb00e3570bf88b130e393
dilsonm <dilson.mascarenhas@gmail.com>
2023-09-16 22:02:17 -0300

    ler parametos do arquivo .env

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-dot-env
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World")
+
+	// carregar exportação do git
+	// go get github.com/joho/godotenv
+
+}
