dd945d31fde227d66b74c1225bed3b463463070f
dilsonm <dilson.mascarenhas@gmail.com>
2023-09-16 23:04:39 -0300

    leitura de variavel ambiente no arquivo .env

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 
-	fmt.Println("Hello World")
+	envErr := godotenv.Load(".env")
 
-	// carregar exportação do git
+	if envErr != nil {
+		fmt.Println("Erro ao carregar arquivo de configuracao.")
+		return
+	}
+
+	envMap, err := godotenv.Read(".env")
+
+	if err != nil {
+		fmt.Println("Erro ao ler variaveis de ambiente de .ENV")
+		return
+	}
+
+	fmt.Printf("%s", envMap["MYMESSAGE"])
+
+	// carregar exportacao do git
 	// go get github.com/joho/godotenv
 
 }
